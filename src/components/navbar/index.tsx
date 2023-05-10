232c44374bd010a355d83a70782e1ff77ff727e7
ŠTEFAN HUŽVÁR <HUZVARS@softec.cz>
2023-05-10 14:52:31 +0200

    add styles for Navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,6 +1,12 @@
+const navbarStyle = {
+  color: 'rgba(250, 250, 250)',
+  padding: '20px',
+  backgroundColor: 'rgba(150, 150, 150)',
+};
+
 const Navbar = () => {
   return (
-    <div>
+    <div style={navbarStyle}>
       <h1>Prehľad trvalých príkazov</h1>
       <h3>Trvalé prikazy</h3>
     </div>
